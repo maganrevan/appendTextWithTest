6b488a593c6b174a47a54ed62752de16af322a10
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2019-06-01 20:28:19 +0200

    Initial Commit

---
+++ appendTextWithTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  appendTextWithTest
+//
+//  Created by Magnus Kruschwitz on 01.06.19.
+//  Copyright © 2019 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
